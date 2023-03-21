9615760f1d00ec4b410730361416d90b5a623f8a
algonchoveriba <nigraalfa141@gmail.com>
2023-03-21 18:11:04 +0900

    ♻️ リファクタリング

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -3,14 +3,12 @@ import {
   ArrowLeftOnRectangleIcon,
   ArrowsRightLeftIcon,
 } from '@heroicons/react/24/solid'
-import { CounterForm } from '@/components/Counter/CounterForm'
-import { CounterList } from '@/components/Counter/CounterList'
-import { BrandForm } from '@/components/Brand/BrandForm'
-import { BrandList } from '@/components/Brand/BrandList'
 import { Session } from '@supabase/supabase-js'
 import { Account } from './Account/Account'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Database } from 'schema'
+import { CounterPanel } from './Counter/CounterPanel'
+import { BrandPanel } from './Brand/BrandPanel'
 
 export const Dashboard = ({ session }: { session: Session }) => {
   const supabase = useSupabaseClient<Database>()
@@ -23,23 +21,11 @@ export const Dashboard = ({ session }: { session: Session }) => {
 
   return (
     <div className="flex snap-x overflow-x-auto">
-      <div className="snap-center">
-        <div className="mx-14 h-96 w-80 rounded-md border border-gray-200/30 bg-gray-200/30 p-4 shadow-lg backdrop-blur-lg">
-          <span className="text-xl font-extrabold">本数登録</span>
-          <CounterForm />
-          <CounterList />
-        </div>
-      </div>
+      <CounterPanel />
       <span className="my-auto">
         <ArrowsRightLeftIcon className="h-10 w-10 text-gray-300" />
       </span>
-      <div className="snap-center">
-        <div className="mx-14 h-96 w-80 rounded-md border border-gray-200/30 bg-gray-200/30 p-4 shadow-lg backdrop-blur-lg">
-          <span className="text-xl font-extrabold">銘柄登録</span>
-          <BrandForm />
-          <BrandList />
-        </div>
-      </div>
+      <BrandPanel />
       <span className="my-auto">
         <ArrowsRightLeftIcon className="h-10 w-10 text-gray-300" />
       </span>
